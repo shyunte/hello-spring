0a5eda0268b0a0ccc84af52bf1861e6dfc477163
Her-ys <heryoonseo92@gmail.com>
2023-02-05 16:46:32 +0900

    스프링 강의

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -12,5 +14,38 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!!");
         return "hello";  //go to hello.html
+    }
+
+    @GetMapping("/hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody     // http에 헤더와 바디 중 바디에 직접 넣겠다는 뜻.
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;   // 데이터를 그대로 내려 줌
+    }
+
+    // responsebody 어노테이션을 사용하면 viewResolver 대신 httpMessageConverter가 실행된다.
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
