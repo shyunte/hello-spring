22ba2ba993d086ebb4ba5d9a8b8482a79bdb1444
hyun12 <sht4441@gmail.com>
2023-02-06 16:04:12 +0900

    last

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,22 +1,42 @@
 package hello.hellospring;
 
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
+       private final MemberRepository memberRepository;
+@Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
     }
-}
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop(){
+//        return new TimeTraceAop();
+//    }
+
+
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+// return new MemoryMemberRepository();
+//        return new JpaMemberRepository(em);
+//
+
+    }
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -5,11 +5,12 @@ import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
-
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -21,30 +22,35 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join(Member member){
-        //중복 이름 x
+
+    public Long join(Member member) {
+
         validateDuplicateMember(member);  //중복회원 검즘
         memberRepository.save(member);
         return member.getId();
+
     }
 
     private void validateDuplicateMember(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
+
     /**
      * 전체회원 조회
      */
 
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
+
+        Long start = System.currentTimeMillis();
+
         return memberRepository.findAll();
 
     }
-    public Optional<Member> findOne(Long memberId){
+
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
-
-
 }
